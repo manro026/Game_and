a31c9a06fa9dcbc467b543dc682625dcf2de8d07
manro026 <icedog26@yandex.ru>
2019-03-14 21:18:26 +0300

    v2

--- test/Assets/Final_script/TEST.cs
+++ test/Assets/Final_script/TEST.cs
@@ -5,51 +5,28 @@ using UnityEngine;
 public class TEST : MonoBehaviour
 {
     public GameObject s;
+    public GameObject s12;
     public Animation clip;
-    private GameObject clone;
     // Start is called before the first frame update
     void Start()
     {
-        clone = Instantiate(s);
-        clone.GetComponent<Animator>();
-
-
-        StartCoroutine(spaw());
-        StartCoroutine(down_list());
+        
     }
 
     private void OnMouseDown()
     {
+        
+        //Instantiate(s, new Vector2(120, 720), Quaternion.identity);
+        s12 = Instantiate(s);
         StartCoroutine(down_list());
+        
     }
-
-    private void Spawn_list()
-    {
-         int select_list = Random.Range(1, 4);
-        switch (select_list)
-        {
-            case 1:
-                Instantiate(s, new Vector2(0, 60), Quaternion.identity);
-                StartCoroutine(down_list());
-                break;
-            case 2:
-                Instantiate(s, new Vector2(0, 60), Quaternion.identity);
-                StartCoroutine(down_list());
-                break;
-            case 3:
-                Instantiate(s, new Vector2(0, 60), Quaternion.identity);
-                StartCoroutine(down_list());
-                break;
-        }
-    }
+    
     IEnumerator down_list()
     {
-        clip.Play("start_down");
-        yield return new WaitForSeconds(1);
-    }
-    IEnumerator spaw()
-    {
-        Instantiate(clone, new Vector2(0, 60), Quaternion.identity);
-        yield return new WaitForSeconds(1);
-    }
+        clip = s12.GetComponent<Animation>();
+        clip.Play("mini_down");
+        yield return new WaitForSeconds(clip.GetClip("mini_down").length);
+        Destroy(s12);
+    }   
 }
